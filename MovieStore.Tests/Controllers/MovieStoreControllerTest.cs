ef89be71a6018245362d944733e3574660cd9b52
IT2030DSatnic <S01031109@acad.tri-c.edu>
2018-05-03 00:13:02 -0400

    Initial commit - to end of video 3 - includes the first Unit Test

---
+++ MovieStore.Tests/Controllers/MovieStoreControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieStore.Controllers;
+using MovieStore.Models;
+
+namespace MovieStore.Tests.Controllers
+{
+    [TestClass]
+    public class MovieStoreControllerTest
+    {
+        [TestMethod]
+        public void MovieStore_Index_TestView()
+        {
+            //Arrange
+            MoviesController controller = new MoviesController();
+
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}

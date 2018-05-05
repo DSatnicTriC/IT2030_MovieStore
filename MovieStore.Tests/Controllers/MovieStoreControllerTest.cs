63b992e47314a6f4a3fabf34ed766ef8b04e818f
IT2030DSatnic <S01031109@acad.tri-c.edu>
2018-05-04 22:52:49 -0400

    To end of video 8 - the details success

--- MovieStore.Tests/Controllers/MovieStoreControllerTest.cs
+++ MovieStore.Tests/Controllers/MovieStoreControllerTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MovieStore.Controllers;
@@ -73,16 +74,62 @@ namespace MovieStore.Tests.Controllers
         {
             //Arrange
             Mock<MovieStoreDbContext> mockContext = new Mock<MovieStoreDbContext>();
-            Mock<DbSet<Movie>> mockSet = new Mock<DbSet<Movie>>();
+            Mock <DbSet<Movie>> mockSet = new Mock<DbSet<Movie>>();
 
 
+            var list = new List<Movie>
+            {
+                new Movie {MovieId = 1, Title = "Jaws"},
+                new Movie {MovieId = 2, Title = "Despicable Me"}
+            }.AsQueryable();
+
+            mockSet.As<IQueryable<Movie>>().Setup(m => m.Provider).Returns(list.Provider);
+            mockSet.As<IQueryable<Movie>>().Setup(m => m.GetEnumerator()).Returns(list.GetEnumerator());
+            mockSet.As<IQueryable<Movie>>().Setup(m => m.Expression).Returns(list.Expression);
+            mockSet.As<IQueryable<Movie>>().Setup(m => m.ElementType).Returns(list.ElementType);
+
+            mockContext.Setup(db => db.Movies).Returns(mockSet.Object);
 
             //Controller needs a mock object for Dependency Injection
             MoviesController controller = new MoviesController(mockContext.Object);
-            
 
             //Act
-            var result = controller.ListFromDb() as ViewResult;
+            ViewResult result = controller.ListFromDb() as ViewResult;
+            List<Movie> resultModel = result.Model as List<Movie>;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Jaws", resultModel[0].Title);
+            Assert.AreEqual("Despicable Me", resultModel[1].Title);
+        }
+
+        [TestMethod]
+        public void MovieStore_Details_Success()
+        {
+            //Arrange
+            Mock<MovieStoreDbContext> mockContext = new Mock<MovieStoreDbContext>();
+            Mock<DbSet<Movie>> mockSet = new Mock<DbSet<Movie>>();
+
+
+            var list = new List<Movie>
+            {
+                new Movie {MovieId = 1, Title = "Jaws"},
+                new Movie {MovieId = 2, Title = "Despicable Me"}
+            }.AsQueryable();
+
+            mockSet.As<IQueryable<Movie>>().Setup(m => m.Provider).Returns(list.Provider);
+            mockSet.As<IQueryable<Movie>>().Setup(m => m.GetEnumerator()).Returns(list.GetEnumerator());
+            mockSet.As<IQueryable<Movie>>().Setup(m => m.Expression).Returns(list.Expression);
+            mockSet.As<IQueryable<Movie>>().Setup(m => m.ElementType).Returns(list.ElementType);
+            mockSet.Setup(m => m.Find(It.IsAny<Object>())).Returns(list.First());
+
+            mockContext.Setup(db => db.Movies).Returns(mockSet.Object);
+
+            //Controller needs a mock object for Dependency Injection
+            MoviesController controller = new MoviesController(mockContext.Object);
+
+            //Act
+            ViewResult result = controller.Details(1) as ViewResult;
 
             //Assert
             Assert.IsNotNull(result);
